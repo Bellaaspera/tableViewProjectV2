884a1571b40489173f88488ce0b27f8a8c381440
Bellaaspera <lanasenatorova@gmail.com>
2022-07-30 13:44:10 +0200

    Initial Commit

---
+++ tableViewProjectV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewProjectV2
+//
+//  Created by Светлана Сенаторова on 30.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
